2a5d9f98245f214670272c8830eec742667788c1
"sathishkumarmsk" <"sathishkumarmsk5699@gmail.com">
2022-08-13 20:29:53 +0530

    array

---
+++ contracts/datatypes.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Primitives {
+    bool public boo = true;uint8 public u8 = 1;
+    uint public u256 = 456;
+    uint public u = 123; 
+}
